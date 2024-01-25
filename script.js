ffd93aacfa499b17cb5658ad560870870c04f1ad
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2024-01-25 15:59:59 +0000

    Annotation e Inference 01

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
+"use strict";
 /* Annotation e Inference */
-
 const produto = 'Livro';
 const preco = 200;
-
 const carro = {
     marca: "Audi",
     portas: 4,
-}
-
+};
 const barato = preco < 400 ? 'Barato' : 'Caro';
-
-type numbers = {
-    a: number;
-    b: number;
-}
-
-function somar({a, b}: numbers) {
+function somar({ a, b }) {
     return a + b;
 }
-
 const nintendo = {
     nome: 'Nintendo',
     preco: "2000",
-}
-
-function transformarPreco(produto: {nome: string, preco: string}) {
+};
+function transformarPreco(produto) {
     produto.preco = `R$ ${produto.preco}`;
     return produto.preco;
 }
-
 console.log(transformarPreco(nintendo));
